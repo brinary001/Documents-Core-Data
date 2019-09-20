2990d0f8ec38608e62da44a779c4de0ebe6c0734
Brian Robinson <br7pc@mail.missouri.edu>
2019-09-19 22:05:34 -0500

    init

---
+++ Documents Core Data/Documents Core Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Documents Core Data
+//
+//  Created by Brian Robinson on 9/19/19.
+//  Copyright © 2019 Brian Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
